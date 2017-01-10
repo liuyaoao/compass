f7d890b4ed5fe6bdd81b1ccff34c75538d408320
liuyaoao <623578381@qq.com>
2017-01-11 01:39:04 +0800

    updatesss

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,6 +3,7 @@
 // 指南针：有两种使用形式，一、指针不动永远指向你面对的方向，罗盘动；二、罗盘不动，指针动，上北下南，左西右东。
 var app = getApp();
 var wezrender = require('../../lib/wezrender');
+var utils = require('../../utils/util');
 var zr = null;
 
 Page({
@@ -10,6 +11,11 @@ Page({
     pixelRatio:0,
     width:0,
     height:0,
+    canvasWidth:0,
+    canvasHeight:0,
+    radius:0,
+    circleX:0,
+    circleY:0,
     userInfo: {}
   },
   //事件处理函数
@@ -29,29 +35,46 @@ Page({
       })
     });
     that.getDeviceSystemInfo();
-    //////////
-    zr = wezrender.zrender.init("compass_canvas", 300, 300);
+    var leftHeight = that.data.height-70;
+    var radius = that.data.width*0.35;
+    if(that.data.width>(leftHeight+50)){
+      radius = (leftHeight-50)/2;
+    }
+    var circleX = that.data.width/2;
+    var circleY = (that.data.height-35-radius)/2;
+    that.setData({
+        canvasWidth:that.data.width,
+        canvasHeight:leftHeight,
+        radius:radius,
+        circleX:circleX,
+        circleY:circleY
+    });
 
+    //////开始画圆形罗盘了。
+    zr = wezrender.zrender.init("compass_canvas", that.data.width, leftHeight);
     var gradient = new wezrender.graphic.LinearGradient();
-    gradient.addColorStop(0, 'red');
-    gradient.addColorStop(1, 'red');
+    gradient.addColorStop(0, '#A8A7AD');
+    gradient.addColorStop(1, '#A8A7AD');
 
     var circle = new wezrender.graphic.shape.Circle({
         shape: {
-            cx: 150,
-            cy: 150,
-            r: that.data.width*0.35
+            cx: circleX,
+            cy: circleY,
+            r: radius
         },
         style: {
+            stroke:"#28A7AD",
             fill: gradient,
-            lineWidth: 10
+            lineWidth: 3
         }
     });
     zr.add(circle);
+    that.drawDirectionText();
+    that.drawScaleDialLine();
 
-        // circle.animate('shape').when(200, {
-        //         cx: 250
-        //     }).start();
+    // circle.animate('shape').when(200, {
+    //     cx: 250
+    // }).start();
             
         // circle.animate('shape', true).when(1000, {
         //         cx: 250
@@ -67,6 +90,47 @@ Page({
         //     }).start();
     ///////////
   },
+  drawScaleDialLine:function(){
+    var that = this;
+    var startX=0,startY=0,endX=0,endY=0;
+    for(var i=1;i<=16;i++){ //分8个刻度
+      var lineColor = "#60E477";
+      var offset = 40;
+      var angle = i*22.5;
+      if(Math.abs(Math.sin(angle*0.017453293))>0.99){
+        // startX = that.data.circleX+(that.data.radius-40)*Math.sin(angle* 0.017453293);
+        // startY = that.data.circleY;
+        // endX = that.data.circleX+(that.data.radius)*Math.sin(angle* 0.017453293);
+        // endY = that.data.circleY;
+      }else if(Math.abs(Math.cos(angle*0.017453293))>0.99){
+        // startX = that.data.circleX;
+        // startY = that.data.circleY-(that.data.radius-40)*Math.cos(angle* 0.017453293);
+        // endX = that.data.circleX;
+        // endY = that.data.circleY-(that.data.radius)*Math.cos(angle* 0.017453293);
+      }else{
+        startX = that.data.circleX+(that.data.radius-20)*Math.sin(angle* 0.017453293);
+        startY = that.data.circleY-(that.data.radius-20)*Math.cos(angle* 0.017453293);
+        endX = that.data.circleX+(that.data.radius)*Math.sin(angle* 0.017453293);
+        endY = that.data.circleY-(that.data.radius)*Math.cos(angle* 0.017453293);
+        lineColor="#786E7B";
+        offset = 20;
+      } 
+      angle+=45;//便偏移0度角。
+      
+        startX = that.data.circleX+(that.data.radius-offset)*Math.sin(angle* 0.017453293);
+        startY = that.data.circleY-(that.data.radius-offset)*Math.cos(angle* 0.017453293);
+        endX = that.data.circleX+(that.data.radius)*Math.sin(angle* 0.017453293);
+        endY = that.data.circleY-(that.data.radius)*Math.cos(angle* 0.017453293);
+      utils.drawLine(zr,wezrender,startX,startY,endX,endY,lineColor);
+    }
+  },
+  drawDirectionText:function(){ //画东南西北四个方向的文字。
+    var that = this;
+    utils.drawText(zr,wezrender,that.data.circleX-12,that.data.circleY-that.data.radius-6,"北");
+    utils.drawText(zr,wezrender,that.data.circleX-12,that.data.circleY+that.data.radius+22,"南");
+    utils.drawText(zr,wezrender,that.data.circleX+that.data.radius,that.data.circleY+6,"东");
+    utils.drawText(zr,wezrender,that.data.circleX-that.data.radius-26,that.data.circleY+6,"西");
+  },
   getDeviceSystemInfo:function(){
     var that = this;
     try {
@@ -76,13 +140,13 @@ Page({
         width:res.windowWidth,
         height:res.windowHeight,
       });
-      // console.log(res.model)
+      console.log(res.model)
       console.log(res.pixelRatio)
       console.log(res.windowWidth)
       console.log(res.windowHeight)
-      // console.log(res.language)
-      // console.log(res.version)
-      // console.log(res.platform)
+      console.log(res.language)
+      console.log(res.version)
+      console.log(res.platform)
     } catch (e) {
       // Do something when catch error
     }
--- utils/util.js
+++ utils/util.js
@@ -11,11 +11,50 @@ function formatTime(date) {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+function drawText(zr,wezrender,x,y,text){
+  var text = new wezrender.graphic.Text({
+            style: {
+                x: x,
+                y: y,
+                text: text,
+                width: 30,
+                height: 30,
+                fill: '#cc00ff',
+                rotation:[20,0,0],
+                textFont: '24px Microsoft Yahei',
+                textBaseline: 'top'
+            }
+        });
+        zr.add(text);
+        return text;
+}
+// 画直线条条。
+function drawLine(zr,wezrender,x1,y1,x2,y2,lineColor){
+  var line = new wezrender.graphic.shape.Line({
+            shape: {
+                x1: x1,
+                y1: y1,
+                x2: x2,
+                y2: y2
+            },
+            style: {
+                lineCap: "round",                   
+                lineWidth: 3,
+                stroke: lineColor,
+            }
+
+        });
+        zr.add(line);
+        return line;
+}
+
 function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  drawText: drawText,
+  drawLine:drawLine
 }
