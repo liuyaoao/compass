7d1e91d70ad994c667a807696785a53684ff4f7e
liuyaoao <623578381@qq.com>
2017-01-12 01:39:54 +0800

    compass finished ..to be optimize...

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,8 @@ Page({
     radius:0,
     circleX:0,
     circleY:0,
+    scaleDialArr:[],
+    directionTextMap:{},
     userInfo: {}
   },
   //事件处理函数
@@ -52,9 +54,9 @@ Page({
 
     //////开始画圆形罗盘了。
     zr = wezrender.zrender.init("compass_canvas", that.data.width, leftHeight);
-    var gradient = new wezrender.graphic.LinearGradient();
-    gradient.addColorStop(0, '#A8A7AD');
-    gradient.addColorStop(1, '#A8A7AD');
+    // var gradient = new wezrender.graphic.LinearGradient();
+    // gradient.addColorStop(0, '#A8A7AD');
+    // gradient.addColorStop(1, '#A8A7AD');
 
     var circle = new wezrender.graphic.shape.Circle({
         shape: {
@@ -64,72 +66,99 @@ Page({
         },
         style: {
             stroke:"#28A7AD",
-            fill: gradient,
+            fill: "#E2DEDE",
             lineWidth: 3
         }
     });
     zr.add(circle);
-    that.drawDirectionText();
-    that.drawScaleDialLine();
-
-    // circle.animate('shape').when(200, {
-    //     cx: 250
-    // }).start();
-            
-        // circle.animate('shape', true).when(1000, {
-        //         cx: 250
-        //     }).when(2000, {
-        //         cx: 250,
-        //         cy: 250
-        //     }).when(3000, {
-        //         cx: 50,
-        //         cy: 250
-        //     }).when(4000, {
-        //         cx: 150,
-        //         cy: 150
-        //     }).start();
-    ///////////
+    
+    that.drawScaleDial(0);
+    var Pin = utils.getCustomGraphPin(wezrender,that.data.circleX,that.data.circleY);
+    that.drawPinPointer(Pin);
+    var flag = 0;
+    wx.onCompassChange(function (res) {
+      // console.log(res.direction)
+      flag++;
+      if(flag%10 == 0){
+        that.drawScaleDial(360-res.direction,true);
+        console.log("direction:"+res.direction)
+      }
+      if(flag == 100000000000){
+        flag=1;
+      }
+    });
   },
-  drawScaleDialLine:function(){
+  drawScaleDial:function(offsetAngle,isUpdate){
     var that = this;
+    var isUpdate = isUpdate || false;
     var startX=0,startY=0,endX=0,endY=0;
-    for(var i=1;i<=16;i++){ //分8个刻度
+    for(var i=1;i<=16;i++){ //分16个刻度
       var lineColor = "#60E477";
       var offset = 40;
       var angle = i*22.5;
       if(Math.abs(Math.sin(angle*0.017453293))>0.99){
-        // startX = that.data.circleX+(that.data.radius-40)*Math.sin(angle* 0.017453293);
-        // startY = that.data.circleY;
-        // endX = that.data.circleX+(that.data.radius)*Math.sin(angle* 0.017453293);
-        // endY = that.data.circleY;
+        if(Math.sin(angle*0.017453293) > 0.99){
+          that.drawDirectionText(angle+offsetAngle,"东",isUpdate);
+        }else if(Math.sin(angle*0.017453293) < -0.99){
+          that.drawDirectionText(angle+offsetAngle,"西",isUpdate);
+        }
       }else if(Math.abs(Math.cos(angle*0.017453293))>0.99){
-        // startX = that.data.circleX;
-        // startY = that.data.circleY-(that.data.radius-40)*Math.cos(angle* 0.017453293);
-        // endX = that.data.circleX;
-        // endY = that.data.circleY-(that.data.radius)*Math.cos(angle* 0.017453293);
+        if(Math.cos(angle*0.017453293) > 0.99){
+          that.drawDirectionText(angle+offsetAngle,"北",isUpdate);
+        }else if(Math.cos(angle*0.017453293) < -0.99){
+          that.drawDirectionText(angle+offsetAngle,"南",isUpdate);
+        }
       }else{
-        startX = that.data.circleX+(that.data.radius-20)*Math.sin(angle* 0.017453293);
-        startY = that.data.circleY-(that.data.radius-20)*Math.cos(angle* 0.017453293);
-        endX = that.data.circleX+(that.data.radius)*Math.sin(angle* 0.017453293);
-        endY = that.data.circleY-(that.data.radius)*Math.cos(angle* 0.017453293);
         lineColor="#786E7B";
         offset = 20;
       } 
-      angle+=45;//便偏移0度角。
-      
-        startX = that.data.circleX+(that.data.radius-offset)*Math.sin(angle* 0.017453293);
-        startY = that.data.circleY-(that.data.radius-offset)*Math.cos(angle* 0.017453293);
-        endX = that.data.circleX+(that.data.radius)*Math.sin(angle* 0.017453293);
-        endY = that.data.circleY-(that.data.radius)*Math.cos(angle* 0.017453293);
-      utils.drawLine(zr,wezrender,startX,startY,endX,endY,lineColor);
+      var tempAngle = angle+offsetAngle;//加上偏移度角。
+      startX = that.data.circleX+(that.data.radius-offset)*Math.sin(tempAngle* 0.017453293);
+      startY = that.data.circleY-(that.data.radius-offset)*Math.cos(tempAngle* 0.017453293);
+      endX = that.data.circleX+(that.data.radius)*Math.sin(tempAngle* 0.017453293);
+      endY = that.data.circleY-(that.data.radius)*Math.cos(tempAngle* 0.017453293);
+      if(isUpdate){
+        that.data.scaleDialArr[i-1].animate('shape').when(10, {
+            x1:startX,
+            y1:startY,
+            x2:endX,
+            y2:endY
+        }).start();
+      }else{
+        console.log("startx,starty,endx,endy:"+startX+","+startY+","+endX+","+endY);
+        that.data.scaleDialArr[i-1] = utils.drawLine(zr,wezrender,startX,startY,endX,endY,lineColor);
+      }
     }
   },
-  drawDirectionText:function(){ //画东南西北四个方向的文字。
+  drawDirectionText:function(tempAngle,direction,isUpdate){ //画东南西北四个方向的文字。
     var that = this;
-    utils.drawText(zr,wezrender,that.data.circleX-12,that.data.circleY-that.data.radius-6,"北");
-    utils.drawText(zr,wezrender,that.data.circleX-12,that.data.circleY+that.data.radius+22,"南");
-    utils.drawText(zr,wezrender,that.data.circleX+that.data.radius,that.data.circleY+6,"东");
-    utils.drawText(zr,wezrender,that.data.circleX-that.data.radius-26,that.data.circleY+6,"西");
+    var tx = that.data.circleX+(that.data.radius)*Math.sin(tempAngle* 0.017453293);
+    var ty = that.data.circleY-(that.data.radius)*Math.cos(tempAngle* 0.017453293);
+    tx += (15)*Math.sin(tempAngle* 0.017453293);
+    ty -= (15)*Math.cos(tempAngle* 0.017453293);
+    tx -= 12;
+    ty += 8;
+    if(isUpdate){
+        that.data.directionTextMap[direction].animate('style').when(10, {
+            x:tx,
+            y:ty
+        }).start();
+      }else{
+        that.data.directionTextMap[direction] = utils.drawText(zr,wezrender,tx,ty,direction);
+      }
+  },
+  drawPinPointer:function(Pin){
+    var that = this;
+    var pin = new Pin({
+        shape: {
+            x: that.data.circleX,
+            y: that.data.circleY,
+            width: 30,
+            height: 80
+        },
+        scale: [1, 1,that.data.circleX,that.data.circleY]
+    });
+    zr.add(pin);
   },
   getDeviceSystemInfo:function(){
     var that = this;
--- utils/util.js
+++ utils/util.js
@@ -10,7 +10,7 @@ function formatTime(date) {
 
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
-
+// 画文本
 function drawText(zr,wezrender,x,y,text){
   var text = new wezrender.graphic.Text({
             style: {
@@ -20,9 +20,10 @@ function drawText(zr,wezrender,x,y,text){
                 width: 30,
                 height: 30,
                 fill: '#cc00ff',
-                rotation:[20,0,0],
+                // rotation:[20,0,0],
+                textAlign:"center",
                 textFont: '24px Microsoft Yahei',
-                textBaseline: 'top'
+                textBaseline: 'middle'
             }
         });
         zr.add(text);
@@ -48,6 +49,59 @@ function drawLine(zr,wezrender,x1,y1,x2,y2,lineColor){
         return line;
 }
 
+// 扩展zrender图形。自定义图形对象。
+function getCustomGraphPin(wezrender,rotx,roty){
+  var Pin = wezrender.graphic.Path.extend({
+            type: 'pin',
+            shape: {
+                // x, y on the cusp
+                x: 0,
+                y: 0,
+                width: 0,
+                height: 0,
+            },
+            rotation:[7,rotx,roty],
+            buildPath: function (path, shape) {
+                var x = shape.x;
+                var y = shape.y;
+                var w = shape.width / 5 * 3;
+                // Height must be larger than width
+                var h = Math.max(w, shape.height);
+                var r = w / 2;
+                // Dist on y with tangent point and circle center
+                var dy = r * r / (h - r);
+                var cy = y - h + r + dy;
+                var angle = Math.asin(dy / r);
+                // Dist on x with tangent point and circle center
+                var dx = Math.cos(angle) * r;
+
+                var tanX = Math.sin(angle);
+                var tanY = Math.cos(angle);
+
+                path.arc(
+                    x, cy, r,
+                    Math.PI - angle,
+                    Math.PI * 2 + angle
+                );
+
+                var cpLen = r * 0.6;
+                var cpLen2 = r * 0.7;
+                path.bezierCurveTo(
+                    x + dx - tanX * cpLen, cy + dy + tanY * cpLen,
+                    x, y - cpLen2,
+                    x, y
+                );
+                path.bezierCurveTo(
+                    x, y - cpLen2,
+                    x - dx + tanX * cpLen, cy + dy + tanY * cpLen,
+                    x - dx, cy + dy
+                );
+                path.closePath();
+            }
+        });
+        return Pin;
+}
+
 function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
@@ -56,5 +110,6 @@ function formatNumber(n) {
 module.exports = {
   formatTime: formatTime,
   drawText: drawText,
-  drawLine:drawLine
+  drawLine:drawLine,
+  getCustomGraphPin:getCustomGraphPin
 }
